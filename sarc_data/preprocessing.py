ae7f3ddd901a2f1c20c181b1d451972955ada485
Wonderplex <50866817+Jasonqi146@users.noreply.github.com>
2024-04-10 18:57:15 -0400

    Exp/abef test logits (#74)
    * changed scripts and bash
    
    * changed setup
    
    * finished splits logits on test

--- sarc_data/preprocessing.py
+++ sarc_data/preprocessing.py
@@ -1,18 +1,17 @@
 import ast
 import os
 
-
-with open("train.txt", 'r') as f:
+with open("raw_data/valid.txt", 'r') as f:
     input_str = f.read()
     lines = [line.strip() for line in input_str.strip().split('\n') if line.strip()]
     lists = [ast.literal_eval(line) for line in lines]
     
-image_root = "../image_data"
+image_root = "raw_data/image_data"
 new_lists = []
-for image_id, sentence, label in lists:
+for image_id, sentence, label, _ in lists:
     if os.path.exists(os.path.join(image_root,'%s.jpg'%image_id)):
         new_lists.append([image_id, sentence, label])
 
-with open("train_filtered.txt", 'w') as f:
+with open("intermediate_data/valid_filtered.txt", 'w') as f:
     for line in new_lists:
         f.write(str(line) + '\n')
