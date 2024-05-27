f7159d259475ae8639672b64304828800092520b
Haofei Yu <haofeiy@cs.cmu.edu>
2024-05-27 18:49:40 -0400

    rebuild blip2 train/test and rebuild sarc and mustard split (#108)
    * rebuild blip2 training and testing
    
    * reorg sarc data
    
    * change the dataset path of mustard

---
+++ data_split/mustard/mustard_split.py
@@ -0,0 +1,84 @@
+import json
+from collections import defaultdict
+
+def read_json_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.readlines()
+
+def parse_labels(reader):
+    label_dict = defaultdict(lambda: {'yes': -float('inf'), 'no': -float('inf'), 'pred': None})
+    for line in reader:
+        identifier, json_str = line.split(' ', 1)
+        data = json.loads(json_str)
+        label_dict[identifier] = parse_prediction(data)
+    return label_dict
+
+def parse_prediction(data):
+    yes_score = data.get('Yes', -float('inf'))
+    no_score = data.get('No', -float('inf'))
+    pred = 1 if yes_score > no_score else 0
+    return {'yes': yes_score, 'no': no_score, 'pred': pred}
+
+def read_vision_and_text_labels():
+    text_label_reader = read_json_file('../../mustard_data/data_gen_output/mustard_text_only_pred.json')
+    vision_label_reader = read_json_file('../../mustard_data/data_gen_output/mustard_vision_only_pred.json')
+    
+    text_label_dict = parse_labels(text_label_reader)
+    vision_label_dict = parse_labels(vision_label_reader)
+    
+    return text_label_dict, vision_label_dict
+
+def read_groundtruth_labels():
+    with open('../../mustard_data/data_raw/mustard_raw_data_speaker_independent_train.json', 'r') as file:
+        dataset = json.load(file)
+        return {key: 1 if value['sarcasm'] else 0 for key, value in dataset.items()}
+
+def select_subset_ids(text_label_dict, vision_label_dict, gth_label_dict):
+    R_ids, AS_ids, U_ids = [], [], []
+    for id, gth_label in gth_label_dict.items():
+        text_label = text_label_dict.get(id, {}).get('pred')
+        vision_label = vision_label_dict.get(id, {}).get('pred')
+        if text_label is None or vision_label is None:
+            continue
+        if text_label == vision_label:
+            (R_ids if text_label == gth_label else AS_ids).append(id)
+        else:
+            U_ids.append(id)
+    return R_ids, AS_ids, U_ids
+
+def construct_subset(ids, dataset):
+    return {id: dataset[id] for id in ids if id in dataset}
+
+def save_dataset(file_path, dataset):
+    with open(file_path, 'w') as file:
+        json.dump(dataset, file)
+
+if __name__ == "__main__":
+    gth_label_dict = read_groundtruth_labels()
+    text_label_dict, vision_label_dict = read_vision_and_text_labels()
+    R_ids, AS_ids, U_ids = select_subset_ids(text_label_dict, vision_label_dict, gth_label_dict)
+    
+    with open('../../mustard_data/data_raw/mustard_raw_data_speaker_independent_train.json', 'r') as file:
+        train_dataset = json.load(file)
+    
+    with open('../../mustard_data/data_raw/mustard_raw_data_speaker_independent_test.json', 'r') as file:
+        test_dataset = json.load(file)
+    
+    train_dataset = construct_subset(train_dataset.keys(), train_dataset)
+    save_dataset('../../mustard_data/data_raw/mustard_dataset_train.json', train_dataset)
+
+    test_dataset = construct_subset(test_dataset.keys(), test_dataset)
+    save_dataset('../../mustard_data/data_raw/mustard_dataset_test.json', test_dataset)
+    
+    R_dataset = construct_subset(R_ids, train_dataset)
+    save_dataset('../../mustard_data/data_split_output/mustard_R_dataset_train.json', R_dataset)
+    
+    AS_dataset = construct_subset(AS_ids, train_dataset)
+    save_dataset('../../mustard_data/data_split_output/mustard_AS_dataset_train.json', AS_dataset)
+    
+    U_dataset = construct_subset(U_ids, train_dataset)
+    save_dataset('../../mustard_data/data_split_output/mustard_U_dataset_train.json', U_dataset)
+
+    print(f"R_ids: {len(R_ids)}")
+    print(f"AS_ids: {len(AS_ids)}")
+    print(f"U_ids: {len(U_ids)}")
---
+++ expert_BLIP2/blip2_train/sarc.py
@@ -0,0 +1,95 @@
+import json
+import torch
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+
+
+class SarcDataset(Dataset):
+    """
+    A custom dataset class that prepares image-text pairs for training.
+    """
+    def __init__(self, dataset_path, tokenizer, image_processor, max_length=128):
+        self.dataset = self.load_dataset(dataset_path)
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.image_processor = image_processor
+
+    def __len__(self):
+        return len(self.dataset)
+    
+    def load_dataset(self, dataset_path):
+        with open(dataset_path) as f:
+            raw_dataset = json.load(f)
+        return [
+            {
+                "image_id": id,
+                "text": data["text"],
+                "label": data["label"]
+            }
+            for id, data in raw_dataset.items()
+        ]
+
+    def __getitem__(self, idx):
+        item = self.dataset[idx]
+        text = item['text']
+        image_path = f'{self.image_data_path}/{item["image_id"]}.jpg'
+        image = Image.open(image_path)
+        image = self.image_processor(image, return_tensors="pt").pixel_values.squeeze(0)
+        label = torch.tensor(item['label'], dtype=torch.long)
+
+        full_prompt = (
+            f"Question: The tweet for this image is: {text}."
+            f"Is the tweet sarcastic (yes or no)? Answer:"
+        )
+
+        text_encoding = self.tokenize_and_left_pad(full_prompt, self.max_length)
+        return { 
+            "input_ids": text_encoding["input_ids"].squeeze(),
+            "attention_mask": text_encoding["attention_mask"].squeeze(),
+            "image": image,
+            "label": label,
+            "tweet": text,
+            "prompt": full_prompt
+        }
+
+    def tokenize_and_left_pad(self, full_prompt, max_length):
+        text_encoding = self.tokenizer(full_prompt, truncation=True, max_length=max_length, return_tensors="pt")
+        seq_len = text_encoding['input_ids'].size(1)
+        padding_length = max_length - seq_len
+        
+        if padding_length > 0:
+            pad_ids = torch.full((1, padding_length), self.tokenizer.pad_token_id, dtype=torch.long)
+            pad_mask = torch.zeros((1, padding_length), dtype=torch.long)
+            text_encoding['input_ids'] = torch.cat([pad_ids, text_encoding['input_ids']], dim=1)
+            text_encoding['attention_mask'] = torch.cat([pad_mask, text_encoding['attention_mask']], dim=1)
+        else:
+            text_encoding['input_ids'] = text_encoding['input_ids'][:, :max_length]
+            text_encoding['attention_mask'] = text_encoding['attention_mask'][:, :max_length]
+        return text_encoding
+
+
+def sarc_collate(batch):
+    """
+    A custom collate function to pad the batches dynamically.
+    """
+    input_ids = torch.stack([item["input_ids"] for item in batch])
+    attention_masks = torch.stack([item["attention_mask"] for item in batch])
+    labels = torch.stack([item["label"] for item in batch])
+    images = torch.stack([item["image"] for item in batch])
+    
+    return {
+        "input_ids": input_ids,
+        "attention_mask": attention_masks,
+        "image": images,
+        "label": labels
+    }
+
+
+def get_sarc_dataloader(args, tokenizer, image_processor, split):
+    if split == "train":
+        dataset = SarcDataset(args.train_path, args.image_data_path, tokenizer, image_processor, args.max_length)
+    elif split == "val":
+        dataset = SarcDataset(args.val_path, args.image_data_path, tokenizer, image_processor, args.max_length)
+    elif split == "test":
+        dataset = SarcDataset(args.test_path, args.image_data_path, tokenizer, image_processor, args.max_length)
+    return DataLoader(dataset, batch_size=args.batch_size, shuffle=True, collate_fn=sarc_collate)
