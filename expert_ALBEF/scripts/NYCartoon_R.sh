11fd1449bcd1d14e60ea8f30b96010bc9a87c2a7
Jasonqi146 <jasonqi146@gmail.com>
2024-06-04 06:30:37 +0000

    finished albef irfl

--- expert_ALBEF/scripts/NYCartoon_R.sh
+++
@@ -1,7 +0,0 @@
-python -m torch.distributed.launch --nproc_per_node=4 --use_env SarcDetect.py \
---config ./configs/SarcDetect_R.yaml \
---output_dir ./output/SarcDetect-R \
---checkpoint ./ALBEF.pth \
---train
-# > ./sarc-detect.log
-# -m torch.distributed.launch --nproc_per_node=8 --use_env
--- expert_ALBEF/scripts/NYCartoon_U_test.sh
+++
@@ -1,5 +0,0 @@
-python -m torch.distributed.launch --nproc_per_node=4 --use_env SarcDetect.py \
---config ./configs/SarcDetect_test.yaml \
---output_dir ./output/SarcDetect-U-test \
---checkpoint ./output/SarcDetect-U/checkpoint_best.pth \
---no-train
