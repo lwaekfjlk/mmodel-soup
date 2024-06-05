401fc8d7693ae13f813b98679f3db4a6d7fec592
Jasonqi146 <jasonqi146@gmail.com>
2024-06-05 07:41:25 +0000

    merged with main

---
+++ expert_ALBEF/run_all.sh
@@ -0,0 +1,8 @@
+bash /root/mmodel-soup/expert_ALBEF/scripts/NYCartoon_AS.sh
+bash /root/mmodel-soup/expert_ALBEF/scripts/NYCartoon_AS_test.sh
+bash /root/mmodel-soup/expert_ALBEF/scripts/NYCartoon_R.sh
+bash /root/mmodel-soup/expert_ALBEF/scripts/NYCartoon_R_test.sh
+bash /root/mmodel-soup/expert_ALBEF/scripts/NYCartoon_U.sh
+bash /root/mmodel-soup/expert_ALBEF/scripts/NYCartoon_U_test.sh
+bash /root/mmodel-soup/expert_ALBEF/scripts/NYCartoon_baseline.sh
+bash /root/mmodel-soup/expert_ALBEF/scripts/NYCartoon_baseline_test.sh
