f32ff91e4ae0bb3283606e32f9b5512e6f90c70c
Wonderplex <50866817+Jasonqi146@users.noreply.github.com>
2024-06-06 11:27:32 +0800

    finished mustard experiments (#135)

--- expert_ALBEF/run_all.sh
+++ expert_ALBEF/run_all.sh
@@ -1,8 +1,8 @@
-bash /root/mmodel-soup/expert_ALBEF/scripts/NYCartoon_AS.sh
-bash /root/mmodel-soup/expert_ALBEF/scripts/NYCartoon_AS_test.sh
-bash /root/mmodel-soup/expert_ALBEF/scripts/NYCartoon_R.sh
-bash /root/mmodel-soup/expert_ALBEF/scripts/NYCartoon_R_test.sh
-bash /root/mmodel-soup/expert_ALBEF/scripts/NYCartoon_U.sh
-bash /root/mmodel-soup/expert_ALBEF/scripts/NYCartoon_U_test.sh
-bash /root/mmodel-soup/expert_ALBEF/scripts/NYCartoon_baseline.sh
-bash /root/mmodel-soup/expert_ALBEF/scripts/NYCartoon_baseline_test.sh
+bash /root/mmodel-soup/expert_ALBEF/scripts/Mustard_AS.sh
+bash /root/mmodel-soup/expert_ALBEF/scripts/Mustard_AS_test.sh
+bash /root/mmodel-soup/expert_ALBEF/scripts/Mustard_R.sh
+bash /root/mmodel-soup/expert_ALBEF/scripts/Mustard_R_test.sh
+bash /root/mmodel-soup/expert_ALBEF/scripts/Mustard_U.sh
+bash /root/mmodel-soup/expert_ALBEF/scripts/Mustard_U_test.sh
+bash /root/mmodel-soup/expert_ALBEF/scripts/Mustard_baseline.sh
+bash /root/mmodel-soup/expert_ALBEF/scripts/Mustard_baseline_test.sh
