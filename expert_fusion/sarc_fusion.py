13aabfb9f29984770ea774eccaff03ab689dc1d8
Jasonqi146 <jasonqi146@gmail.com>
2024-06-09 10:37:52 -0400

    added sarc data and scripts

--- expert_fusion/sarc_fusion.py
+++ expert_fusion/sarc_fusion.py
@@ -4,6 +4,25 @@ import os
 import jsonlines
 from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
 
+def load_and_transform_baseline(file_dir):
+    subset_names = ['baseline']
+    dataset = defaultdict(list)
+    results = defaultdict(lambda: {'logits': defaultdict(list), 'target': None})
+
+    # Load data from files
+    for name in subset_names:
+        file_path = os.path.join(file_dir, f'sarc_{name}_logits.jsonl')
+        with jsonlines.open(file_path, 'r') as f:
+            for line in f:
+                image_id = line['image_id']
+                data_id = f"{image_id}"
+                dataset[name].append(line)
+                results[data_id]['logits'][name] = line['logits']
+                if results[data_id]['target'] is None:
+                    results[data_id]['target'] = line['target']
+                assert results[data_id]['target'] == line['target'], "Targets do not match across subsets for the same data."
+    return dataset, results
+
 def load_and_transform_data(file_dir):
     subset_names = ['AS', 'R', 'U']
     dataset = defaultdict(list)
@@ -11,7 +30,7 @@ def load_and_transform_data(file_dir):
 
     # Load data from files
     for name in subset_names:
-        file_path = os.path.join(file_dir, f'sarc_vision_text_label_test_{name}.jsonl')
+        file_path = os.path.join(file_dir, f'sarc_{name}_logits.jsonl')
         with jsonlines.open(file_path, 'r') as f:
             for line in f:
                 image_id = line['image_id']
@@ -100,7 +119,7 @@ def cascaded_fusion(results, threshold):
 
 # Example usage within your main workflow
 if __name__ == "__main__":
-    file_dir = '../../sarc_data/intermediate_data/ALBEF_RUS_outputs'
+    file_dir = '../sarc_data/expert_inference_output/expert_albef'
     _, transformed_results = load_and_transform_data(file_dir)
 
     weights = {'AS': 0.0, 'R': 0.2, 'U': 0.2}
@@ -115,3 +134,5 @@ if __name__ == "__main__":
     print("AS Interaction Type Accuracy:", interaction_type_acc(transformed_results, 'AS'))
     print("R Interaction Type Accuracy:", interaction_type_acc(transformed_results, 'R'))
     print("U Interaction Type Accuracy:", interaction_type_acc(transformed_results, 'U'))
+    baseline_dataset, baseline_results = load_and_transform_baseline(file_dir)
+    print("Baseline Interaction Type Accuracy:", interaction_type_acc(baseline_results, 'baseline'))
