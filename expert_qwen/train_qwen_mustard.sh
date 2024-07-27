8485b558bca3bcc3f2862c059e6d542f0318e9ca
ljang0 <ljang@andrew.cmu.edu>
2024-07-27 16:32:44 -0400

    updated qwen code

--- expert_qwen/train_qwen_mustard.sh
+++ expert_qwen/train_qwen_mustard.sh
@@ -1,55 +1,55 @@
-#python train.py \
-#--dataset mustard \
-#--train_path ../mustard_data/data_split_output/mustard_AS_dataset_train.json \
-#--val_path ../mustard_data/data_raw/mustard_dataset_test.json \
-#--test_path ../mustard_data/data_raw/mustard_dataset_test.json \
-#--image_data_path ../mustard_data/data_raw/images \
-#--save_path ./0.5_qwen_mustard_AS_model \
-#--model_size 0.5 \
-#--batch_size 1 \
-#--eval_steps 25 \
-#--epochs 5 \
-#--device 2 \
-#--max_length 512;
-
-#python train.py \
-#--dataset mustard \
-#--train_path ../mustard_data/data_split_output/mustard_R_dataset_train.json \
-#--val_path ../mustard_data/data_raw/mustard_dataset_test.json \
-#--test_path ../mustard_data/data_raw/mustard_dataset_test.json \
-#--image_data_path ../mustard_data/data_raw/images \
-#--save_path ./0.5_qwen_mustard_R_model \
-#--model_size 0.5 \
-#--batch_size 1 \
-#--eval_steps 10 \
-#--epochs 5 \
-#--device 2 \
-#--max_length 512;
-
-#python train_new.py \
-#--dataset mustard \
-#--train_path ../mustard_data/data_split_output/mustard_U_dataset_train.json \
-#--val_path ../mustard_data/data_raw/mustard_dataset_test.json \
-#--test_path ../mustard_data/data_raw/mustard_dataset_test.json \
-#--image_data_path ../mustard_data/data_raw/images \
-#--save_path ./0.5_qwen_mustard_U_model \
-#--model_size 0.5 \
-#--batch_size 1 \
-#--eval_steps 10 \
-#--device 2 \
-#--max_length 512;
-
-
 python train.py \
 --dataset mustard \
---train_path ../mustard_data/data_raw/mustard_dataset_train.json \
+--train_path /storage/mmodel-soup/new_data_splits/mustard_data_split_output/mustard_AS_dataset_train_cogvlm2_qwen2.json \
 --val_path ../mustard_data/data_raw/mustard_dataset_test.json \
 --test_path ../mustard_data/data_raw/mustard_dataset_test.json \
 --image_data_path ../mustard_data/data_raw/images \
---save_path ./0.5_qwen_mustard_baseline_model \
---model_size 0.5 \
+--save_path ./1.5_qwen_mustard_AS_new_model \
+--model_size 1.5 \
+--batch_size 1 \
+--eval_steps 25 \
+--epochs 5 \
+--device 2 \
+--max_length 512;
+
+python train.py \
+--dataset mustard \
+--train_path /storage/mmodel-soup/new_data_splits/mustard_data_split_output/mustard_R_dataset_train_cogvlm2_qwen2.json \
+--val_path ../mustard_data/data_raw/mustard_dataset_test.json \
+--test_path ../mustard_data/data_raw/mustard_dataset_test.json \
+--image_data_path ../mustard_data/data_raw/images \
+--save_path ./1.5_qwen_mustard_R_new_model \
+--model_size 1.5 \
 --batch_size 1 \
 --eval_steps 10 \
 --epochs 5 \
 --device 2 \
 --max_length 512;
+
+python train.py \
+--dataset mustard \
+--train_path /storage/mmodel-soup/new_data_splits/mustard_data_split_output/mustard_U_dataset_train_cogvlm2_qwen2.json \
+--val_path ../mustard_data/data_raw/mustard_dataset_test.json \
+--test_path ../mustard_data/data_raw/mustard_dataset_test.json \
+--image_data_path ../mustard_data/data_raw/images \
+--save_path ./1.5_qwen_mustard_U_new_model \
+--model_size 1.5 \
+--batch_size 1 \
+--eval_steps 10 \
+--device 2 \
+--max_length 512;
+
+
+# python train.py \
+# --dataset mustard \
+# --train_path ../mustard_data/data_raw/mustard_dataset_train.json \
+# --val_path ../mustard_data/data_raw/mustard_dataset_test.json \
+# --test_path ../mustard_data/data_raw/mustard_dataset_test.json \
+# --image_data_path ../mustard_data/data_raw/images \
+# --save_path ./1.5_qwen_mustard_baseline_model \
+# --model_size 1.5 \
+# --batch_size 1 \
+# --eval_steps 10 \
+# --epochs 5 \
+# --device 2 \
+# --max_length 512;
