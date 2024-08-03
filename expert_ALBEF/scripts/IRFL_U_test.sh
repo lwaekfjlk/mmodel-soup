909b24ff85c5dbc0e7df5107295b0508519f0a8a
Wonderplex <50866817+Jasonqi146@users.noreply.github.com>
2024-08-02 17:12:53 -0700

    removed useless configs and scripts in albef (#171)

--- expert_ALBEF/scripts/IRFL_U_test.sh
+++
@@ -1,5 +0,0 @@
-python -m torch.distributed.launch --nproc_per_node=1 --use_env IRFL.py \
---config ./configs/IRFL_test.yaml \
---output_dir ./output/IRFL-U \
---checkpoint ./output/IRFL-U/checkpoint_best.pth \
---no-train
--- expert_ALBEF/scripts/NYCartoon_U.sh
+++
@@ -1,5 +0,0 @@
-python -m torch.distributed.launch --nproc_per_node=1 --use_env NYCartoon.py \
---config ./configs/NYCartoon_U.yaml \
---output_dir ./output/NYCartoon-U \
---checkpoint ./ALBEF.pth \
---train
