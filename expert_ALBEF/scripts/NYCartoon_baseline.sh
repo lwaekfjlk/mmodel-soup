d306c8b6b404d74c93a560beb4fbbacb67ac343b
Wonderplex <50866817+Jasonqi146@users.noreply.github.com>
2024-08-02 17:12:53 -0700

    removed useless configs and scripts in albef (#171)

--- expert_ALBEF/scripts/NYCartoon_baseline.sh
+++
@@ -1,5 +0,0 @@
-python -m torch.distributed.launch --nproc_per_node=1 --use_env NYCartoon.py \
---config ./configs/NYCartoon.yaml \
---output_dir ./output/NYCartoon \
---checkpoint ./ALBEF.pth \
---train
