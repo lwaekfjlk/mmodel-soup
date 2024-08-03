3784f5f2b279e105603f534dcb1d64752cc83a90
Wonderplex <50866817+Jasonqi146@users.noreply.github.com>
2024-08-02 17:12:53 -0700

    removed useless configs and scripts in albef (#171)

--- expert_ALBEF/scripts/NYCartoon_AS.sh
+++
@@ -1,7 +0,0 @@
-python -m torch.distributed.launch --nproc_per_node=1 --use_env NYCartoon.py \
---config ./configs/NYCartoon_AS.yaml \
---output_dir ./output/NYCartoon-AS \
---checkpoint ./ALBEF.pth \
---train
-# > ./sarc-detect.log
-# -m torch.distributed.launch --nproc_per_node=8 --use_env
--- expert_ALBEF/scripts/NYCartoon_AS_test.sh
+++
@@ -1,5 +0,0 @@
-python -m torch.distributed.launch --nproc_per_node=1 --use_env NYCartoon.py \
---config ./configs/NYCartoon_test.yaml \
---output_dir ./output/NYCartoon-AS \
---checkpoint ./output/NYCartoon-AS/checkpoint_best.pth \
---no-train
