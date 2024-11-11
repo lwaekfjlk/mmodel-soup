26db222107b5a31d5ff4ddd37472603ec493a7cb
Jasonqi146 <jasonqi146@gmail.com>
2024-11-10 21:57:12 -0800

    deleted mmsd1.0 from albef

--- expert_ALBEF/scripts/SarcDetect_U.sh
+++
@@ -1,7 +0,0 @@
-python -m torch.distributed.launch --nproc_per_node=5 --use_env SarcDetect.py \
---config ./configs/SarcDetect_U.yaml \
---output_dir ./output/SarcDetect-U \
---checkpoint ./ALBEF.pth \
---train
-# > ./sarc-detect.log
-# -m torch.distributed.launch --nproc_per_node=8 --use_env
