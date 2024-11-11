37710c15883eb5f9389821f5bcf35355e0a0b110
Haofei Yu <haofeiy@cs.cmu.edu>
2024-11-11 00:38:48 -0600

    feat(fusion): add model fusion (#180)
    * update all data
    
    * update with full raw data
    
    * update
    
    * update
    
    * add empty image folder
    
    * update data split
    
    * support data fusion

--- expert_fusion/urfunny_fusion_train.sh
+++ expert_fusion/urfunny_fusion_train.sh
@@ -1,8 +1,8 @@
-python blip2_fusion_train.py \
+jbsub -mem 80g -cores 20+1 -q alt_24h -require h100 -out ./urfunny-focal-loss.log -err ./urfunny-focal-loss.err python blip2_fusion_train.py \
 --dataset urfunny \
 --image_data_path ../urfunny_data/data_raw/images \
---save_path ./blip2_fuser \
---batch_size 10 \
---eval_steps 100 \
---epochs 10 \
---max_length 512;
+--save_path ./urfunny_blip2_fuser_focal_loss \
+--batch_size 50 \
+--eval_steps 70 \
+--epochs 50 \
+--max_length 512
