c83f133455a144381c9316fd9bea992ef48006a1
Haofei Yu <haofeiy@cs.cmu.edu>
2024-11-11 03:25:16 -0600

    feat(pre-commit): update with pre-commit (#192)

--- data_gen_vision_label_CogVLM2/convert_vision_label_format_for_fuser.py
+++ data_gen_vision_label_CogVLM2/convert_vision_label_format_for_fuser.py
@@ -1,35 +1,44 @@
-import jsonlines
 import json
-from typing import Dict, List
-from sklearn.metrics import f1_score
 from collections import Counter
+from typing import Dict, List
+
+import jsonlines
+from sklearn.metrics import f1_score
+
 
 def apply_thresholds(results: Dict, thresholds: List[float]) -> Dict[float, float]:
     return {
         threshold: f1_score(
-            [value['gth'] for value in results.values() if value['gth'] is not None],
-            [1 if value['logits']['Yes'] >= threshold else 0 for value in results.values() if value['gth'] is not None]
+            [value["gth"] for value in results.values() if value["gth"] is not None],
+            [
+                1 if value["logits"]["Yes"] >= threshold else 0
+                for value in results.values()
+                if value["gth"] is not None
+            ],
         )
         for threshold in thresholds
     }
 
+
 def add_pred_based_on_threshold(results: Dict, threshold: float) -> Dict:
     for key, value in results.items():
-        if value['logits'] and value['gth'] is not None:
-            value['pred'] = 1 if value['logits']['Yes'] >= threshold else 0
+        if value["logits"] and value["gth"] is not None:
+            value["pred"] = 1 if value["logits"]["Yes"] >= threshold else 0
         else:
-            value['pred'] = None
-    return {k: v for k, v in results.items() if v['gth'] is not None}
+            value["pred"] = None
+    return {k: v for k, v in results.items() if v["gth"] is not None}
+
 
 def save_results_to_json(results, file_path):
-    with open(file_path, 'w') as f:
+    with open(file_path, "w") as f:
         json.dump(results, f, indent=4)
+
 
 def get_prediction(results, balance_lower_bound):
     pred_counter = Counter()
     for result in results.values():
-        result['pred'] = 1 if result['logits']['Yes'] > result['logits']['No'] else 0
-        pred_counter[result['pred']] += 1
+        result["pred"] = 1 if result["logits"]["Yes"] > result["logits"]["No"] else 0
+        pred_counter[result["pred"]] += 1
 
     pred_ratio = pred_counter[1] / sum(pred_counter.values())
     if pred_ratio < balance_lower_bound:
@@ -37,79 +46,87 @@ def get_prediction(results, balance_lower_bound):
     elif pred_ratio > 1 - balance_lower_bound:
         results = select_top_percent_as_one(results, 1 - balance_lower_bound)
 
-    print(Counter(result['pred'] for result in results.values()))
+    print(Counter(result["pred"] for result in results.values()))
     return results
 
+
 def select_top_percent_as_one(results, percentage):
-    sorted_logits = sorted(results.items(), key=lambda x: x[1]['logits']['Yes'], reverse=True)
+    sorted_logits = sorted(
+        results.items(), key=lambda x: x[1]["logits"]["Yes"], reverse=True
+    )
     threshold_index = int(len(sorted_logits) * percentage)
 
     for i, (image_id, _) in enumerate(sorted_logits):
-        results[image_id]['pred'] = 1 if i < threshold_index else 0
+        results[image_id]["pred"] = 1 if i < threshold_index else 0
     return results
+
 
 def mask_uncertain_predictions(results, threshold):
     train_count = 0
     for value in results.values():
-        if value['logits'] and value['gth'] is not None:
-            prob_diff = abs(value['logits']['Yes'] - value['logits']['No'])
+        if value["logits"] and value["gth"] is not None:
+            prob_diff = abs(value["logits"]["Yes"] - value["logits"]["No"])
             should_include_in_train = prob_diff >= threshold
-            value['should_include_in_train'] = should_include_in_train
+            value["should_include_in_train"] = should_include_in_train
             train_count += should_include_in_train
-    print(f'Train count: {train_count}')
+    print(f"Train count: {train_count}")
     return results
+
 
 def process_data(file_path, ids, split):
     with jsonlines.open(file_path) as reader:
         labels = list(reader)
-    
+
     dataset = {}
     for data in labels:
-        if data['image_id'] in ids:
-            yes_probs = data['logits']['Yes'] / (data['logits']['Yes'] + data['logits']['No'])
-            no_probs = data['logits']['No'] / (data['logits']['Yes'] + data['logits']['No'])
-            dataset[data['image_id']] = {
-                'logits': data['logits'],
-                'gth': data.get('gth')
+        if data["image_id"] in ids:
+            dataset[data["image_id"]] = {
+                "logits": data["logits"],
+                "gth": data.get("gth"),
             }
     return dataset
 
+
 def compute_f1(results):
-    preds = [value['pred'] for value in results.values() if value['gth'] is not None]
-    gths = [value['gth'] for value in results.values() if value['gth'] is not None]
+    preds = [value["pred"] for value in results.values() if value["gth"] is not None]
+    gths = [value["gth"] for value in results.values() if value["gth"] is not None]
     return f1_score(gths, preds)
 
+
 if __name__ == "__main__":
+    dataset_name = "urfunny"
 
-    dataset_name = 'urfunny'
-
-    with open(f'../{dataset_name}_data/data_raw/{dataset_name}_dataset_train.json') as f:
+    with open(
+        f"../{dataset_name}_data/data_raw/{dataset_name}_dataset_train.json"
+    ) as f:
         train_ids = list(json.load(f).keys())
 
-    with open(f'../{dataset_name}_data/data_raw/{dataset_name}_dataset_val.json') as f:
+    with open(f"../{dataset_name}_data/data_raw/{dataset_name}_dataset_val.json") as f:
         val_ids = list(json.load(f).keys())
 
-    with open(f'../{dataset_name}_data/data_raw/{dataset_name}_dataset_test.json') as f:
+    with open(f"../{dataset_name}_data/data_raw/{dataset_name}_dataset_test.json") as f:
         test_ids = list(json.load(f).keys())
 
     train_results = process_data(
-        f'../{dataset_name}_data/data_gen_output/{dataset_name}_image_only_pred_cogvlm2.jsonl',
+        f"../{dataset_name}_data/data_gen_output/{dataset_name}_image_only_pred_cogvlm2.jsonl",
         train_ids,
-        'train'
+        "train",
     )
 
     train_results = get_prediction(train_results, 0.0)
-    print(f'Train F1 score: {compute_f1(train_results)}')
+    print(f"Train F1 score: {compute_f1(train_results)}")
 
     other_results = process_data(
-        f'../{dataset_name}_data/data_gen_output/{dataset_name}_image_only_pred_cogvlm2.jsonl',
+        f"../{dataset_name}_data/data_gen_output/{dataset_name}_image_only_pred_cogvlm2.jsonl",
         val_ids + test_ids,
-        'other'
+        "other",
     )
 
     other_results = get_prediction(other_results, 0.0)
-    print(f'Other F1 score: {compute_f1(other_results)}')
-
+    print(f"Other F1 score: {compute_f1(other_results)}")
 
     results = {**train_results, **other_results}
-    save_results_to_json(results, f'../{dataset_name}_data/data_gen_output/{dataset_name}_image_only_pred_cogvlm2_for_fuser.json')
+    save_results_to_json(
+        results,
+        f"../{dataset_name}_data/data_gen_output/{dataset_name}_image_only_pred_cogvlm2_for_fuser.json",
+    )
--- data_gen_vision_label_CogVLM2/urfunny_vision_label.py
+++ data_gen_vision_label_CogVLM2/urfunny_vision_label.py
@@ -1,16 +1,17 @@
 import os
 import time
+
 import torch
-import json
-from transformers import AutoModelForCausalLM, AutoTokenizer
 from tqdm import tqdm
-from utils import recur_move_to, collate_fn, load_ground_truth_labels, load_images, prepare_input_samples, save_results
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from utils import (collate_fn, load_ground_truth_labels, load_images,
+                   prepare_input_samples, recur_move_to, save_results)
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 MODEL_PATH = "/dataset/granite_ckpt/haofeiyu/cogvlm2-llama3-chat-19B"
 TORCH_TYPE = torch.bfloat16
-device = 'cuda'
+device = "cuda"
 
 image_folder = "../urfunny_data/data_raw/images"
 data_folder = "../urfunny_data/data_raw"
@@ -31,7 +32,14 @@ query = (
     "If you are not sure, please provide your best guess and do not say that you are not sure."
 )
 
-ground_truth_labels = load_ground_truth_labels(data_folder, ["urfunny_dataset_train.json", "urfunny_dataset_val.json", "urfunny_dataset_test.json"])
+ground_truth_labels = load_ground_truth_labels(
+    data_folder,
+    [
+        "urfunny_dataset_train.json",
+        "urfunny_dataset_val.json",
+        "urfunny_dataset_test.json",
+    ],
+)
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH, trust_remote_code=True)
 
@@ -40,7 +48,9 @@ yes_token_id = tokenizer.convert_tokens_to_ids(yes_token)
 no_token = tokenizer.tokenize("No")
 no_token_id = tokenizer.convert_tokens_to_ids(no_token)
 
-model = AutoModelForCausalLM.from_pretrained(MODEL_PATH, torch_dtype=TORCH_TYPE, trust_remote_code=True, device_map=device).eval()
+model = AutoModelForCausalLM.from_pretrained(
+    MODEL_PATH, torch_dtype=TORCH_TYPE, trust_remote_code=True, device_map=device
+).eval()
 
 data = load_images(image_folder)
 
@@ -48,15 +58,25 @@ length = len(data)
 print(length)
 print(len(ground_truth_labels))
 
-for idx in tqdm(range(0, length, batch_size), desc="Processing", total=length // batch_size):
+for idx in tqdm(
+    range(0, length, batch_size), desc="Processing", total=length // batch_size
+):
     i_list = [data[idx + i]["image"] for i in range(batch_size) if idx + i < length]
 
-    input_sample_list, image_id_list = prepare_input_samples(model, tokenizer, query, i_list)
+    input_sample_list, image_id_list = prepare_input_samples(
+        model, tokenizer, query, i_list
+    )
 
     start = time.time()
     input_batch = collate_fn(input_sample_list, tokenizer)
-    input_batch = recur_move_to(input_batch, device, lambda x: isinstance(x, torch.Tensor))
-    input_batch = recur_move_to(input_batch, torch.bfloat16, lambda x: isinstance(x, torch.Tensor) and torch.is_floating_point(x))
+    input_batch = recur_move_to(
+        input_batch, device, lambda x: isinstance(x, torch.Tensor)
+    )
+    input_batch = recur_move_to(
+        input_batch,
+        torch.bfloat16,
+        lambda x: isinstance(x, torch.Tensor) and torch.is_floating_point(x),
+    )
     print(f"Prepare batch time: {time.time() - start}")
 
     start = time.time()
