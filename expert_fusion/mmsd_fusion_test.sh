0d9a9e0e9bcecf19ed3c2e35c2dbf2c11f1b4626
Haofei Yu <haofeiy@cs.cmu.edu>
2024-11-11 00:38:48 -0600

    feat(fusion): add model fusion (#180)
    * update all data
    
    * update with full raw data
    
    * update
    
    * update
    
    * add empty image folder
    
    * update data split
    
    * support data fusion

--- expert_fusion/mmsd_fusion_test.sh
+++ expert_fusion/mmsd_fusion_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-python blip2_fusion_train.py \
+python blip2_fusion_train_with_classifier.py \
 --mode test \
---test_dataset mmsd \
+--dataset mmsd \
 --image_data_path ../mmsd_data/data_raw/images \
---load_model_name blip2_fuser \
---batch_size 10 \
+--load_model_name mmsd_blip2_fuser_with_classifier_A100_multi-task_best_val_loss \
+--val_batch_size 60 \
 --eval_steps 10 \
 --epochs 5 \
---max_length 512;
+--max_length 512
