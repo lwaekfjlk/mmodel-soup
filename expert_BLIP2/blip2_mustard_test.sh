e37714121d88eab203c178acdacf16f4de6630aa
Haofei Yu <haofeiy@cs.cmu.edu>
2024-11-11 01:07:13 -0600

    feat(expert): support blip2 (#186)

--- expert_BLIP2/blip2_mustard_test.sh
+++ expert_BLIP2/blip2_mustard_test.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
 
-python train.py \
+export SEED=1;
+jbsub -mem 80g -cores 20+1 -q alt_24h -require h100 -out ./mustard-test-${SEED}.log -err ./mustard-test-${SEED}.err python train.py \
 --mode test \
 --test_dataset mustard \
 --test_path ../mustard_data/data_raw/mustard_dataset_test.json \
 --image_data_path ../mustard_data/data_raw/images \
---load_model_name blip2_sarc_mustard_nycartoon_irfl_baseline_model \
+--load_model_name blip2_mustard_baseline_model_${SEED} \
 --batch_size 10 \
 --eval_steps 10 \
 --epochs 5 \
---max_length 512;
+--max_length 512
 
-python train.py \
+export SEED=3;
+jbsub -mem 80g -cores 20+1 -q alt_24h -require h100 -out ./mustard-r-test-${SEED}.log -err ./mustard-r-test-${SEED}.err python train.py \
 --mode test \
 --test_dataset mustard \
 --test_path ../mustard_data/data_raw/mustard_dataset_test.json \
 --image_data_path ../mustard_data/data_raw/images \
---load_model_name blip2_sarc_mustard_nycartoon_irfl_R_model \
+--load_model_name blip2_mustard_R_model_${SEED} \
 --batch_size 10 \
 --eval_steps 10 \
 --epochs 5 \
---max_length 512;
+--max_length 512
 
-python train.py \
+export SEED=3;
+jbsub -mem 80g -cores 20+1 -q alt_24h -require h100 -out ./mustard-u-test-${SEED}.log -err ./mustard-u-test-${SEED}.err python train.py \
 --mode test \
 --test_dataset mustard \
 --test_path ../mustard_data/data_raw/mustard_dataset_test.json \
 --image_data_path ../mustard_data/data_raw/images \
---load_model_name blip2_sarc_mustard_nycartoon_irfl_U_model \
+--load_model_name blip2_mustard_U_model_${SEED} \
 --batch_size 10 \
 --eval_steps 10 \
 --epochs 5 \
---max_length 512;
+--max_length 512
 
-python train.py \
+export SEED=3;
+jbsub -mem 80g -cores 20+1 -q alt_24h -require h100 -out ./mustard-as-test-${SEED}.log -err ./mustard-as-test-${SEED}.err  python train.py \
 --mode test \
 --test_dataset mustard \
 --test_path ../mustard_data/data_raw/mustard_dataset_test.json \
 --image_data_path ../mustard_data/data_raw/images \
---load_model_name blip2_sarc_mustard_nycartoon_irfl_AS_model \
+--load_model_name blip2_mustard_AS_model_${SEED} \
 --batch_size 10 \
 --eval_steps 10 \
 --epochs 5 \
---max_length 512;
+--max_length 512
