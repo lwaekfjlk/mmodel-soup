473f951f0ad5b3f62f1d8bfa4ef08425b2b93543
lwaekfjlk <1125027232@qq.com>
2024-11-11 07:05:17 +0000

    support blip2

--- expert_BLIP2/blip2_urfunny_test.sh
+++ expert_BLIP2/blip2_urfunny_test.sh
@@ -1,43 +1,49 @@
-python train.py \
---mode test \
---test_dataset urfunny \
---test_path ../urfunny_data/data_raw/urfunny_dataset_test.json \
---image_data_path ../urfunny_data/data_raw/images \
---load_model_name blip2_urfunny_baseline_model \
---batch_size 10 \
---eval_steps 10 \
---epochs 5 \
---max_length 512;
+#!/bin/bash
 
-python train.py \
+export SEED=1111;
+jbsub -mem 80g -cores 20+1 -q alt_24h -require h100 -out ./urfunny-test-${SEED}.log -err ./urfunny-test-${SEED}.err python train.py \
 --mode test \
 --test_dataset urfunny \
 --test_path ../urfunny_data/data_raw/urfunny_dataset_test.json \
 --image_data_path ../urfunny_data/data_raw/images \
---load_model_name blip2_urfunny_R_model \
+--load_model_name blip2_urfunny_baseline_model_${SEED} \
 --batch_size 10 \
 --eval_steps 10 \
 --epochs 5 \
---max_length 512;
+--max_length 512
 
-python train.py \
+export SEED=1111;
+jbsub -mem 80g -cores 20+1 -q alt_24h -require h100 -out ./urfunny-r-test-${SEED}.log -err ./urfunny-r-test-${SEED}.err python train.py \
 --mode test \
 --test_dataset urfunny \
 --test_path ../urfunny_data/data_raw/urfunny_dataset_test.json \
 --image_data_path ../urfunny_data/data_raw/images \
---load_model_name blip2_urfunny_U_model \
+--load_model_name blip2_urfunny_R_model_${SEED} \
 --batch_size 10 \
 --eval_steps 10 \
 --epochs 5 \
---max_length 512;
+--max_length 512
 
-python train.py \
+export SEED=1111;
+jbsub -mem 80g -cores 20+1 -q alt_24h -require h100 -out ./urfunny-u-test-${SEED}.log -err ./urfunny-u-test-${SEED}.err python train.py \
 --mode test \
 --test_dataset urfunny \
 --test_path ../urfunny_data/data_raw/urfunny_dataset_test.json \
 --image_data_path ../urfunny_data/data_raw/images \
---load_model_name blip2_urfunny_AS_model \
+--load_model_name blip2_urfunny_U_model_${SEED} \
 --batch_size 10 \
 --eval_steps 10 \
 --epochs 5 \
---max_length 512;
+--max_length 512
+
+export SEED=1111;
+jbsub -mem 80g -cores 20+1 -q alt_24h -require h100 -out ./urfunny-as-test-${SEED}.log -err ./urfunny-as-test-${SEED}.err  python train.py \
+--mode test \
+--test_dataset urfunny \
+--test_path ../urfunny_data/data_raw/urfunny_dataset_test.json \
+--image_data_path ../urfunny_data/data_raw/images \
+--load_model_name blip2_urfunny_AS_model_${SEED} \
+--batch_size 10 \
+--eval_steps 10 \
+--epochs 5 \
+--max_length 512
